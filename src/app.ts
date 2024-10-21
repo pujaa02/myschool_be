e78b5e1af9b69b265507a2829daa7f4f70ed282a
Puja Moravadiya <puja.moravadiya2024@gmail.com>
2024-10-21 12:05:14 +0530

    app.ts file create

---
+++ src/app.ts
@@ -0,0 +1,71 @@
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+import { NODE_ENV, PORT } from './config';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import db from './models';
+import { Transaction } from 'sequelize';
+import { Routes } from './common/interfaces/general/routes.interface';
+import errorMiddleware from './middlewares/error.middleware';
+import { logger } from './common/util/logger';
+import * as http from 'http';
+
+const app: Application = express();
+const env: string = NODE_ENV || 'development';
+const port: string | number = PORT || 8000;
+
+function initializeMiddleWares() {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  //   app.use(express.json());
+  app.use(cors({ credentials: true, origin: true }));
+  app.use(cookieParser());
+
+  app.use((req, res, next) => {
+    global.currentRequest = req;
+    global.currentResponse = res;
+    next();
+  });
+
+  app.use(async (req, res, next) => {
+    const method = req.method.toLowerCase();
+    if (method !== 'get') {
+      const t: Transaction = await db.transaction();
+      req.transaction = t;
+    }
+
+    next();
+  });
+}
+function initializeRoutes(routes: Routes[]) {
+  routes.forEach((route) => {
+    app.use('/api/v1', route.router);
+  });
+}
+
+function initializeErrorHandling() {
+  app.use(errorMiddleware);
+}
+
+function handle404() {
+  app.use((req, res) => {
+    if (req.transaction) {
+      req.transaction.rollback();
+    }
+
+    res.status(500).send('Something went wrong');
+  });
+}
+export const initializeApp = async (apiRoutes: Routes[]) => {
+  initializeMiddleWares();
+  initializeRoutes(apiRoutes);
+  initializeErrorHandling();
+  handle404();
+  const server = http.createServer(app);
+  //   socket.connect(server);
+  server.listen(port, () => {
+    logger.info(`=================================`);
+    logger.info(`======= ENV: ${env} ========`);
+    logger.info(`=================================`);
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,27 @@
-import express, { Application } from 'express';
-const app: Application = express();
-import bodyParser from 'body-parser';
 import db from './models';
-import { PORT } from './config';
 import { logger } from './common/util/logger';
-const port = PORT || 3000;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+import AuthRoute from './modules/auth/routes/auth.route';
+import UserRoute from './modules/user/routes/user.route';
+import FeatureRoute from './modules/feature/routes/feature.route';
+import PermissionRoute from './modules/permission/routes/permission.route';
+import RoleRoute from './modules/role/routes/role.route';
+import RolePermissionRoute from './modules/rolePermission/routes/rolePermission.route';
+import { initializeApp } from './app';
+
+const routes = [
+  new AuthRoute(),
+  new FeatureRoute(),
+  new PermissionRoute(),
+  new RoleRoute(),
+  new RolePermissionRoute(),
+  new UserRoute(),
+];
 
 const main = async () => {
   try {
     await db.authenticate();
+    const apiRoutes = routes;
+    await initializeApp(apiRoutes);
   } catch (err) {
     logger.error('[SERVER START]: %s', err);
     process.exit(1);
