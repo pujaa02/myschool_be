0d62dc72204b1332ab9a552833c472c7ca61c88c
Puja Moravadiya <puja.moravadiya2024@gmail.com>
2024-11-05 17:06:03 +0530

    added

--- src/common/lib/query/querySetter/rawQuery.helper.ts
+++ src/common/lib/query/querySetter/rawQuery.helper.ts
@@ -1,28 +1,28 @@
-import { queryCases } from '@/common/constants/enum.constants';
+// import { queryCases } from '../../../../common/constants/enum.constant';
 
-export const sequelizeQuerySetter = (_req?: Request, cases?: string, _data = {}) => {
-  let query = '';
-  switch (cases) {
-    case queryCases.exampleQuery:
-      query += ` (select count(*) as userCount`;
-      query += ` From `;
-      query += ` Users )`;
-      break;
+// export const sequelizeQuerySetter = (_req?: Request, cases?: string, _data = {}) => {
+//   let query = '';
+//   switch (cases) {
+//     case queryCases.exampleQuery:
+//       query += ` (select count(*) as userCount`;
+//       query += ` From `;
+//       query += ` Users )`;
+//       break;
 
-    case queryCases.exampleQuery1:
-      query += ` (select count(*) as userCount1`;
-      query += ` From `;
-      query += ` Users )`;
-      break;
+//     case queryCases.exampleQuery1:
+//       query += ` (select count(*) as userCount1`;
+//       query += ` From `;
+//       query += ` Users )`;
+//       break;
 
-    case queryCases.exampleQuery2:
-      query += ` (select count(*) as userCount2`;
-      query += ` From `;
-      query += ` Users )`;
-      break;
-    default:
-      break;
-  }
+//     case queryCases.exampleQuery2:
+//       query += ` (select count(*) as userCount2`;
+//       query += ` From `;
+//       query += ` Users )`;
+//       break;
+//     default:
+//       break;
+//   }
 
-  return query;
-};
+//   return query;
+// };
--- src/middlewares/checkAccess.middleware.ts
+++ src/middlewares/checkAccess.middleware.ts
@@ -1,45 +1,40 @@
+// import { FeaturesEnum, PermissionEnum, RoleEnum } from '@/common/constants/enum.constant';
+// import { HttpException } from '@/common/helper/response/httpException';
+// import { catchAsync, parseData } from '@/common/util';
+// import { USER_STATUS } from '@/models/interfaces/user.model.interface';
+// import Role from '@/models/role.model';
+// import User from '@/models/user.model';
+// import { Request, RequestHandler } from 'express';
+// import { parse } from 'path';
+// import { Op } from 'sequelize';
 
-import { FeaturesEnum, PermissionEnum, RoleEnum } from '@/common/constants/enum.constant';
-import { HttpException } from '@/common/helper/response/httpException';
-import { catchAsync, parseData } from '@/common/util';
-import { USER_STATUS } from '@/models/interfaces/user.model.interface';
-import Role from '@/models/role.model';
-import User from '@/models/user.model';
-import { Request, RequestHandler } from 'express';
-import { parse } from 'path';
-import { Op } from 'sequelize';
+// interface DynamicFieldOptions {
+//   [key: string]: any;
+// }
+// const checkAccessMiddleware = (feature: FeaturesEnum, permission: PermissionEnum, checkDataId?: string): RequestHandler => {
+//   return catchAsync(async (req: Request, res, next) => {
+//     const accessQuery: DynamicFieldOptions = {
+//       user_id: req?.tokenData?.user?.id,
+//       language: req?.language,
+//     };
+//     let model: any;
+//     let checkId: string = '';
 
-interface DynamicFieldOptions {
-  [key: string]: any;
-}
-const checkAccessMiddleware = (feature: FeaturesEnum, permission: PermissionEnum, checkDataId?: string): RequestHandler => {
-  return catchAsync(async (req: Request, res, next) => {
-    const accessQuery: DynamicFieldOptions = {
-      user_id: req?.tokenData?.user?.id,
-      language: req?.language,
-    };
-    let model: any;
-    let checkId: string = '';
+//     const courseBundleId =
+//       checkDataId == 'bundleId'
+//         ? req?.body?.bundle?.id
+//         : checkDataId === 'bundle_id'
+//           ? req?.query?.bundle_id
+//           : checkDataId === 'id'
+//             ? req?.body?.id
+//             : '';
 
-    const courseBundleId =
-      checkDataId == 'bundleId'
-        ? req?.body?.bundle?.id
-        : checkDataId === 'bundle_id'
-          ? req?.query?.bundle_id
-          : checkDataId === 'id'
-            ? req?.body?.id
-            : '';
+//     })
+// };
 
-   
-    })
-};
+// export const checkCreatedBy = async (modelName: any, userId: number, checkId: string) => {
+//   const resultData = await modelName.findOne({ where: { created_by: userId, id: checkId } }).then((e) => e?.id);
+//   if (resultData) return resultData;
+// };
 
-
-
-export const checkCreatedBy = async (modelName: any, userId: number, checkId: string) => {
-  const resultData = await modelName.findOne({ where: { created_by: userId, id: checkId } }).then((e) => e?.id);
-  if (resultData) return resultData;
-};
-
-
-export default checkAccessMiddleware;
+// export default checkAccessMiddleware;
