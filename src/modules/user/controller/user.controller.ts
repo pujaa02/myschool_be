a0554a6a4af2fc9dd4398a159370dee8d9798c96
Puja Moravadiya <puja.moravadiya2024@gmail.com>
2024-11-13 12:21:55 +0530

    modifiy getloggin api

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -5,76 +5,117 @@ import { catchAsync } from '../../../common/util';
 import { generalResponse } from '../../../common/helper/response/generalResponse';
 import User from '../../../models/user.model';
 import { queryBuildCases } from '../../../common/constants/enum.constant';
-import { getAllDetails, getDetail } from '../../../common/lib/query/querySetter/database.helper';
+import { getAllDetails } from '../../../common/lib/query/querySetter/database.helper';
 import Role from '../../../models/role.model';
-import UserOrganization from '../../../models/userOrganization.model';
 import UserOrganizationRepo from '../../../modules/userOrganization/repository/userOrganization.repository';
+import { Sequelize } from 'sequelize';
+import Permission from '../../../models/permission.model';
+import Feature from '../../../models/feature.model';
+import RolePermissionRepo from '../../../modules/rolePermission/repository/rolePermission.repository';
+import RoleRepo from '../../../modules/role/repository/role.repository';
+import PermissionRepo from '../../../modules/permission/repository/permission.repository';
 
 export default class UserController {
   private readonly userRepository: UserRepo = new UserRepo();
   public userOrganizationRepo = new UserOrganizationRepo();
+  private rolePermissionRepository = new RolePermissionRepo();
+  private roleRepository = new RoleRepo();
+  private permissionRepository = new PermissionRepo();
   constructor() {
     // do nothing.
   }
 
   public readonly getLoggedInUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { id, organization_id } = req.tokenData;
-      const user: User & { organization?: UserOrganization } = await this.userRepository.get({
-        where: { id },
-        include: [
-          {
-            model: Role.scope({ method: ['defaultOrganization', organization_id] }),
-            required: false,
-            include: [
-              {
-                model: Role.scope({ method: ['defaultOrganization', organization_id] }),
-                attributes: ['id', 'name'],
-                required: false,
-              },
-            ],
-          },
-        ],
-        rejectOnEmpty: false,
-      });
-
-      const organizations = await this.userOrganizationRepo.getAll({
-        where: { user_id: id },
-        include: [
-          {
-            model: User,
-            attributes: ['id', 'first_name', 'last_name'],
-            required: true,
-          },
-        ],
-      });
-
-      // const permissions = await this.permissionGroupRepository.getPermissions({
-      //   role_id: user.user_roles?.[0]?.role_id,
-      //   organization_id,
-      //   withoutAll: false,
-      // });
-
-      // permissions.forEach((obj) => {
-      //   obj.permissions = obj.permissions.map((permission) => {
-      //     permission.status = _.clone(permission.role_permissions[0].status);
-      //     return permission;
-      //   });
-      // });
-
-      return generalResponse(res, {
-        user: {
-          ...user.toJSON(),
-          organization: organizations.find((obj) => obj.id === organization_id),
-          user_organizations: organizations,
+    const user = req.tokenData;
+    const roleAndPermission = await this.rolePermissionRepository.getAll({
+      include: [
+        {
+          model: Feature,
+          attributes: [],
         },
-        two_factor_enabled: req.tokenData.two_factor_enabled,
-        two_factor_verified: req.tokenData.two_factor_verified,
-        // permissions,
-      });
-    } catch (error) {
-      return next(error);
-    }
+        {
+          model: Permission,
+          attributes: [],
+        },
+        {
+          model: Role,
+          attributes: [],
+        },
+      ],
+      attributes: [
+        'access',
+        [Sequelize.col('feature.name'), 'feature_name'],
+        [Sequelize.col('role.name'), 'role_name'],
+        [Sequelize.col('feature.id'), 'feature_id'],
+        [Sequelize.col('permission.name'), 'permission_name'],
+        [Sequelize.col('permission.id'), 'permission_id'],
+      ],
+      where: {
+        role_id: user.role.role_id,
+      },
+    });
+    const role = await this.roleRepository.getAll({});
+
+    const permission = await this.permissionRepository.getAll({});
+
+    return generalResponse(res, { user, roleAndPermission, role, permission }, 'USER_FETCHED');
+    // try {
+    //   const { id, organization_id } = req.tokenData;
+    //   const user: User & { organization?: UserOrganization } = await this.userRepository.get({
+    //     where: { id },
+    //     include: [
+    //       {
+    //         model: Role.scope({ method: ['defaultOrganization', organization_id] }),
+    //         required: false,
+    //         include: [
+    //           {
+    //             model: Role.scope({ method: ['defaultOrganization', organization_id] }),
+    //             attributes: ['id', 'name'],
+    //             required: false,
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //     rejectOnEmpty: false,
+    //   });
+
+    //   const organizations = await this.userOrganizationRepo.getAll({
+    //     where: { user_id: id },
+    //     include: [
+    //       {
+    //         model: User,
+    //         attributes: ['id', 'first_name', 'last_name'],
+    //         required: true,
+    //       },
+    //     ],
+    //   });
+
+    //   // const permissions = await this.permissionGroupRepository.getPermissions({
+    //   //   role_id: user.user_roles?.[0]?.role_id,
+    //   //   organization_id,
+    //   //   withoutAll: false,
+    //   // });
+
+    //   // permissions.forEach((obj) => {
+    //   //   obj.permissions = obj.permissions.map((permission) => {
+    //   //     permission.status = _.clone(permission.role_permissions[0].status);
+    //   //     return permission;
+    //   //   });
+    //   // });
+
+    //   return generalResponse(res, {
+    //     user: {
+    //       ...user.toJSON(),
+    //       organization: organizations.find((obj) => obj.id === organization_id),
+    //       user_organizations: organizations,
+    //     },
+    //     two_factor_enabled: req.tokenData.two_factor_enabled,
+    //     two_factor_verified: req.tokenData.two_factor_verified,
+    //     // permissions,
+    //   });
+    // } catch (error) {
+    //   return next(error);
+    // }
   });
 
   public readonly createUser = catchAsync(async (req: Request, res: Response) => {
